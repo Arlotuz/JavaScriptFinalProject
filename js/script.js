9c5ce5e4b7c8208bb38ab6759a23d3cbed250b8a
Arlotuz <74227413+Arlotuz@users.noreply.github.com>
2024-02-20 13:58:52 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,10 +11,11 @@ function pickImage(){
 function setImages(){
     options = ["bacon_in_the_woods.png", "hollow_tree.png", "bend_in_the_road.png","BaconGallupPark.jpg"]
     currentImages = document.querySelectorAll(".flex img")
-    for (var i = 0; i < xs.length; i++) { 
+    for (var i = 0; i < currentImages.length; i++) { 
         randomImg = "images/" + options[Math.floor(Math.random() * options.length)];
-        i.setAttribute("src",randomImg)
-        i.setAttribute("alt","")    
+        //i.setAttribute("src",randomImg);
+        //i.setAttribute("alt","");
+        currentImages[i].src = randomImg;
     }
 
 }
